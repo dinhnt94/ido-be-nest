bc6370f6586248a3799e057246bb5427b8f73e3a
phat-senspark <phatnt@senspark.com>
2022-06-22 18:57:21 +0700

    fix port

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,14 @@
-import { Controller, Get, Param, Put } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get, Param, Put, Req, Res,  } from '@nestjs/common';
+import { Response } from 'express';
 
-@Controller('')
+@Controller()
 export class AppController {
   constructor() {}
 
   // Helth check
   @Get()
-  getDummy(): string {
-    return 'Hello, Dummy!!!';
+  getDummy(@Req() req, @Res() res: Response) {
+    console.log("Hello dummy")
+    return res.status(200).send("Hello dummy");
   }
 }
