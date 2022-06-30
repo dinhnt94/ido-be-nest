693fd03a45e6d9ed11542f67fec82e4bdcb59e96
Dinh Nguyen <dinhnt@senspark.com>
2022-06-30 09:06:18 +0700

    update test
    Signed-off-by: Dinh Nguyen <dinhnt@senspark.com>

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -2,9 +2,9 @@ export const StakeBCOINIDOInfo = {
     name: "Guaranteed Round: Stake BCOIN",
     token: 'BOMB',
     type: 'IDO',
-    timeStartIDO: [1656497394, 1656500400],
-    timeWhiteList: [1656500400, 1656502200],
-    timeClaimIDO: 1656415800,
+    timeStartIDO: [1656556200, 1656557100],
+    timeWhiteList: [1656557100, 1656558000],
+    timeClaimIDO: 1656474300,
     status: false,
     event: 2
 }
@@ -13,9 +13,9 @@ export const CommunityIDOInfo = {
     name: "Community Round : Random users",
     token: 'BOMB',
     type: 'IDO',
-    timeStartIDO: [1656497394, 1656500400],
-    timeWhiteList: [1656500400, 1656502200],
-    timeClaimIDO: 1656415800,
+    timeStartIDO: [1656556200, 1656557100],
+    timeWhiteList: [1656557100, 1656558000],
+    timeClaimIDO: 1656474300,
     status: false,
     event: 1
 }
@@ -24,9 +24,9 @@ export const AutionIDOInfo = {
     name: "Presale BOMB Token Event",
     token: 'BOMB',
     type: 'IDO',
-    timeStartIDO: [1656497394, 1656500400],
-    timeWhiteList: [1656500400, 1656502200],
-    timeClaimIDO: 1656415800,
+    timeStartIDO: [1656556200, 1656557100],
+    timeWhiteList: [1656557100, 1656556500],
+    timeClaimIDO: 1656474300,
     status: false,
     event: 3
 }
@@ -35,9 +35,9 @@ export const StakeSENIDOInfo = {
     name: "Guaranteed Round: Stake SEN",
     token: 'BOMB',
     type: 'IDO',
-    timeStartIDO: [1656497394, 1656500400],
-    timeWhiteList: [1656500400, 1656502200],
-    timeClaimIDO: 1656415800,
+    timeStartIDO: [1656556200, 1656557100],
+    timeWhiteList: [1656557100, 1656558000],
+    timeClaimIDO: 1656474300,
     status: false,
     event: 4
 }
